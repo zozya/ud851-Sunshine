7df53af758b48da26c256179826ea0b7236f8e05
zozya <zozyq@web.de>
2017-12-26 21:53:58 +0100

    S06.01-Exercise-LaunchSettingsActivity ver 1

---
+++ S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/SettingsActivity.java
@@ -0,0 +1,30 @@
+package com.example.android.sunshine;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        // Do step 2 in SettingsActivity
+        // DONE (2) Set setDisplayHomeAsUpEnabled to true on the support ActionBar
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
